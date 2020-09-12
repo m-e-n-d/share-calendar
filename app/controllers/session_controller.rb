df5afa5be446d86ec6c67b41de4f8a6b0074f827
aoking21 <ayky3305@gmail.com>
2020-09-12 13:33:20 +0900

    ログインページを見れるようにする
    /loginでログインページにアクセスできます。

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -1,17 +1,15 @@
 class SessionController < ApplicationController
-  before_action :set_user, only: [:create]
+  def new
+  end
 
   def create
+  end
 
+  def destroy
   end
 end
 
 private
-
-  def set_user
-    @user = User.find_by(user_name: login_params[:user_name], password: login_params[:password])
-  end
-
   def login_params
     params.require(:login_params).permit(:user_name, :password)
   end
