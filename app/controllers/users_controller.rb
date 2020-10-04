668ac97ed890b3f7ce1f996a5f3a485563e0631b
aoking21 <ayky3305@gmail.com>
2020-10-04 10:40:34 +0900

    ログイン機能を追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -69,6 +69,6 @@ class UsersController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def user_params
-      params.require(:user).permit(:name, :password, :display_name)
+      params.require(:user).permit(:name, :password_digest, :display_name)
     end
 end
--- app/helpers/session_helper.rb
+++ app/helpers/session_helper.rb
@@ -1,2 +1,24 @@
 module SessionHelper
+  # ユーザー情報でログインする
+  def login(user)
+    session[:user_name] = user.name
+  end
+
+  # 現在ログインしているユーザーを返す
+  def current_user
+    if session[:user_name]
+      @current_user ||= User.find_by(name: session[:user_name])
+    end
+  end
+
+  # ログイン済みかを返す
+  def logged_in?
+    !current_user.nil?
+  end
+
+  # ログアウトする
+  def log_out
+    session.delete(:user_name)
+    @current_user = nil
+  end
 end
